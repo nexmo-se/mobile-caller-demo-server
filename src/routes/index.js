d2da1f6970b4904928ea15296caed282897777be
Yee Poh <yee.poh@vonage.com>
2020-06-19 16:29:47 +0800

    add opentok route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import pushRouter from './push';
+import opentokRouter from './opentok';
 
 const router = express.Router();
 
 router.get('/', (_, res) => res.send('root'));
 router.use('/push', pushRouter);
+router.use('/opentok', opentokRouter);
 
 export default router;
